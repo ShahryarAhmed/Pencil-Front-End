6e13f5418476f706fa686aa1483704c9d8c2f20a
shahryarahmed <ahmedshahryar333@gmail.com>
2021-11-27 18:40:33 +0300

    initial commit

---
+++ Downloads/canva-master/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB686SlYFXR42UQVpWk0x2_-qM_C9x0GGk",
+    authDomain: "ahmed-44609.firebaseapp.com",
+    projectId: "ahmed-44609",
+    storageBucket: "ahmed-44609.appspot.com",
+    messagingSenderId: "1061359452479",
+    appId: "1:1061359452479:web:15ac6fc2d72d8b0e53938a"
+  },
+};
